5debf7f2cc34b16823243d2aa994b6e649d0c5c9
pmc0707 <pranjal.mridul_cs22@gla.ac.in>
2024-09-02 15:19:20 +0530

    insertion sort

---
+++ src/sorting/bubbleSort/InsertionSort.java
@@ -0,0 +1,46 @@
+package sorting.bubbleSort;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        insertionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
